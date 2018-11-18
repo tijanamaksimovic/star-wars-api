91071b839babd74c54cdb2a2c373cce9feced1c4
Tijana Maksimovic <tijana.maksimovic@toolsense.io>
2018-11-18 19:57:27 +0100

    Initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="logo">
+        <img alt="Star Wars logo" src="../assets/star-wars-logo.png">
+    </div>
+    <div class="input"></div>
+    <div class="table"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
